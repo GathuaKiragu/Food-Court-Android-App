ece8e8462c35e131c6a293a027fc9560e8ba3991
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-07 10:13:44 +0300

    Initial Commit Authentification with Firebase

---
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -0,0 +1,18 @@
+package com.epicodus.myrestaurants;
+
+/**
+ * Created by kiragu on 5/29/17.
+ */
+
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_ACCESS_TOKEN = BuildConfig.YELP_ACCESS_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=food";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+    public static final String PREFERENCES_LOCATION_KEY= "location";
+    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
+    public static final String FIREBASE_CHILD_RESTAURANTS = "restaurants"; //restaurants node key
+
+}
