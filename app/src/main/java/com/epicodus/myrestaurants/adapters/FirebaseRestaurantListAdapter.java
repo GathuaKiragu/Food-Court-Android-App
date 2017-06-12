7629601cb504896ddf1fb953a7f7a28c8ddb9631
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-12 14:58:47 +0300

    Added A FirebaseRestaurantAdaoter

---
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -0,0 +1,8 @@
+package com.epicodus.myrestaurants.adapters;
+
+/**
+ * Created by kiragu on 6/12/17.
+ */
+
+public class FirebaseRestaurantListAdapter {
+}
--- app/src/main/java/com/epicodus/myrestaurants/util/OnStartDragListener.java
+++ app/src/main/java/com/epicodus/myrestaurants/util/OnStartDragListener.java
@@ -1,8 +1,13 @@
 package com.epicodus.myrestaurants.util;
 
+import android.support.v7.widget.RecyclerView;
+
+import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
+
 /**
  * Created by kiragu on 6/11/17.
  */
 
 public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
 }
